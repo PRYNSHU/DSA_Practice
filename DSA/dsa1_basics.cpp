6197a4a2a5109910c20e74307f252a52ad54f4cd
PRIYANSHU <4prianshu2051@gmail.com>
2022-12-27 13:56:18 +0530

    commit & push

--- DSA/dsa1_basics.cpp
+++ DSA/dsa1_basics.cpp
@@ -197,18 +197,18 @@ using namespace std;
     
 // }
 
-int main(){
-    int arr1[6]= {1, 2, 2, 2, 3, 4};
-    int arr2[4] = { 2,20,3,3};
-    for (int i=0;i<6;i++){
-        for (int j=0;j<4;j++){
-            if(arr1[i]==arr2[j]){
-                cout << arr1[i] << " ";
-                break;
-            }
-        }
-    }
-}
+// int main(){
+//     int arr1[6]= {1, 2, 2, 2, 3, 4};
+//     int arr2[4] = { 2,20,3,3};
+//     for (int i=0;i<6;i++){
+//         for (int j=0;j<4;j++){
+//             if(arr1[i]==arr2[j]){
+//                 cout << arr1[i] << " ";
+//                 break;
+//             }
+//         }
+//     }
+// }
 
 
 
