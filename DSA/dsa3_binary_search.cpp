01d0b11f6d9ee121d7313516f65444fa1ebc7ca6
PRIYANSHU <4prianshu2051@gmail.com>
2022-12-29 02:38:11 +0530

    commit and push

--- DSA/dsa3_binary_search.cpp
+++ DSA/dsa3_binary_search.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int binarySearch(int arr[], int size , int key){
-    int s=0, e= size-1;
+    int s=0, e= size-1; //start and end are index values
     int mid = (s+e)/2;
 
     while(s<=e){
@@ -22,11 +22,12 @@ int binarySearch(int arr[], int size , int key){
 }
 
 int main(){
-    int even[6] = {2,4,6,12,33,56};
+    int even[6] = {2,4, (int)6.8 ,12,33,56};  //just an experiment
     int odd[7] = {1,2,3,4,5,6,7};
 
     int ans = binarySearch(odd , 7 , 6);
-    cout << ans ;
+    int ans2 = binarySearch(even , 6, 4);
+    cout << ans << " " << ans2;
 }
 
 
---
+++ code_test/test.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int t,N,count ;
+//     string S;
+// 	cin >> t;
+	
+// 	for(int j=1;j<=t;j++){
+// 	    cin >> N;
+// 	    cin >> S;
+//         count=0;
+// 	    for(int i=0;i<N/2;i++){
+// 	        if(S[i]==S[(N/2)+i]){
+// 	            count +=1;
+//             }
+            
+// 	    }
+//         if(count == N/2){
+//             cout << "YES";
+//         }
+//         else{
+//             cout << "NO";
+//         }
+//             cout << endl;
+// 	}
+// 	return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int t,n,i;
+//     cin >> t;
+// 	while(t--){
+// 	    cin >>n;
+//         int a[n];
+//         for (int i=0;i<n;i++){
+//             cin >> a[i];
+//         }
+//         int m= INT16_MIN;
+//         for(int i=0;i<n;i++){
+//             if(m<a[i]){
+//                 m=a[i];
+//             }
+//         }cout << m <<endl;
+// 	}
+// 	return 0;
+// }
+
+
+int firstOcc(int arr[], int n, int k){
+    int s =0, ans ;
+    int e= n-1;
+    int mid = s + (e-s)/2;
+    while(s<=e){ 
+        if(arr[mid]==k){
+            ans = mid;
+            e= mid-1;
+        }
+        else if(arr[mid]<k){
+            s= mid+1;
+        }
+        else{ e = mid-1;}
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+int lastOcc(int arr[], int n, int k){
+    int s =0, ans ;
+    int e= n-1;
+    int mid = s + (e-s)/2;
+    while(s<=e){ 
+        if(arr[mid]==k){
+            ans = mid;
+            s= mid +1;
+        }
+        else if(arr[mid]<k){
+            s= mid+1;
+        }
+        else{ e = mid-1;}
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main(){
+    int a[] = {1,2,2,2,2,2,3,3,3,3,5,6,7};
+    cout << "firstOcc" << firstOcc(a , 13 , 2) <<endl;
+    cout << "lastOcc" << lastOcc(a , 13 , 2);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
