b103aa7b9c913e09743a705739bef1b79da6d61f
PRIYANSHU <4prianshu2051@gmail.com>
2022-12-30 22:19:54 +0530

    commit

--- DSA/dsa5_sorting.cpp
+++ DSA/dsa5_sorting.cpp
@@ -2,23 +2,24 @@
 #include "vector"
 using namespace std;
 
-
 int main(){
     vector<int> arr ={6,4,2,10,5};
+    vector<int > g;
     int n =5;
     int minIndex;
-    for(int i=0;i<n-1;i++){
-        minIndex = i;
+    for(int i=0;i<=n-1;i++){
+        minIndex = arr[i];
 
-        for (int j= i+1;j<n;j++){
+        for (int j= i+1;j<=n;j++){
 
-            if(arr[j]< arr[minIndex])
-            minIndex = j;
-
+            if(arr[j]< minIndex)
+            minIndex = arr[j];
+            
         }
-        swap(arr[minIndex] , arr[i]);
+        swap(minIndex , arr[i]);
+        g.push_back(arr[i]);
     }
-    for(int v: arr){
+    for(int v: g){
         cout << v << " ";
     }
    
