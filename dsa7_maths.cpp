b7261fda871a0260ee6d30e9d2c17d4c62d04b37
PRIYANSHU <4prianshu2051@gmail.com>
2023-01-10 00:37:37 +0530

    final commit

---
+++ dsa7_maths.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int gcd(int a ,int b){
+//     if(a==0)
+//     return b;
+
+//     if(b==0)
+//     return a;
+
+//     while(a!=b){
+//         if(a>b)
+//         a=a-b;
+//         else
+//         b=b-a;
+//     }
+//     return a;
+
+
+// }
+// int main(){
+//     int a, b;
+//     cin >> a >> b;
+
+//     int ans = gcd(a ,b);
+//     cout << "ans is " << ans ;
+// }
+
+
+
