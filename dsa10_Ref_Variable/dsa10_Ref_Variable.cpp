9b8959c8a14a249fc8b13331e4980e5aa0b617d2
PRIYANSHU <4prianshu2051@gmail.com>
2023-01-20 03:13:48 +0530

    Ref_Variable

--- dsa10_Ref_Variable/dsa10_Ref_Variable.cpp
+++ dsa10_Ref_Variable/dsa10_Ref_Variable.cpp
@@ -1,9 +1,9 @@
-
 #include <iostream>
 using namespace std;
+
 void ref(){
     int i =8;
-    //creating a ref variable 
+    //creating a reference variable 
     int &j = i;  //i and j points same memory location
     cout << i << " " << j << endl;
 
@@ -23,34 +23,35 @@ void update2(int &a){  //pass by reference
 int main(){
     //ref();
     
-    int n =5;
-    cout << "before " << n << endl;
-    update1(n);  //pass by value (n is copy in the fun)
-    cout << "after " << n << endl;
+    // int n =5;
+    // cout << "before " << n << endl;
+    // update1(n);  //pass by value (n is copy in the fun)
+    // cout << "after " << n << endl;
 
-    update2(n);  //pass by reference 
-    //(new a variable is created ,but pointing to n of main function)
-    cout << "after: " << n << endl;
+    // update2(n);  //pass by reference
+    // //(new a variable is created, but pointing to n of main function)
+    // cout << "after: " << n << endl;
 
-    //dynamic memory allocation
+    //DYNAMIC MEMORY ALLOCATION
     int *i = new int;
     cout << i << endl;
 
     //variable size array
-    // int m;
-    // cin >> m;
-    // //arr array pointed to (array of variable size in heap) 
-    // int *arr = new int[m];
+    int m;
+    cin >> m;
+    //arr array pointed to (array of variable size in heap) 
+    int *arr = new int[m];
 
-    // //case 1
-    // while(true){ //static memory allocated (delete and create...)
-    //     int i= 5;
-    // }
+    //case 1
+    while(true){ //static memory allocated (delete and create...)
+        int i= 5;
+    }
     
-    // //case 2
-    // while (true){  //dynamic memory allocation 
-    //     int *arr = new int[40];
-    // }
+    //case 2
+    while (true){  //dynamic memory allocation 
+        int *arr = new int[40];
+    }
     int *c = new int ;
     delete c; //to delete dynamic memory
+
 }
