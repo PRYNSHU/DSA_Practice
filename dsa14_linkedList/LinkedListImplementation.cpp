f9e3c517efa39422b31f3ebb2264b03cdb1c140a
PRIYANSHU <4prianshu2051@gmail.com>
2023-03-30 01:19:07 +0530

    Linked_List

--- dsa14_linkedList/LinkedListImplementation.cpp
+++ dsa14_linkedList/LinkedListImplementation.cpp
@@ -8,39 +8,46 @@ class Node {
     int value; //data 
     Node *next; //node data type ka pointer
 
-  Node(int val){
-    // (*this).value = val;
-    this->value = val;
-    this->next = NULL;
-  }
+  //can also be done by para. const.
+  // Node(int val){
+  //   // (*this).value = val;
+  //   this->value = val;
+  //   this->next = NULL;
+  // }
+  
 };
 
 int main() {
 
   Node* head;
-  //Node* one = NULL;
+  Node* one = NULL;
   Node* two = NULL;
   Node* three = NULL;
 
   // allocate 3 nodes in the heap (dynamic memory allocation) & 
-  //also assigning the values by para const. 
-  Node *one = new Node(1);
-  two = new Node(2);
-  three = new Node(4);
+  //also assigning the values by para const.
+  one = new Node();
+  two = new Node();
+  three = new Node();
 
   // Assign value values
   // (*one).value = 1;
-  // one->value = 1;
-  // two->value = 2;
-  // three->value = 3;
+  one->value = 1;
+  two->value = 2;
+  three->value = 3;
 
   // Connect nodes
   // (*one).next = two;
   one->next = two;
   two->next = three;
   three->next = NULL;
-
+  
+  //IMP -> one , two , three are all pointers of Node class data type
+  //pointing to heap memory.
+  int *p = new int;
+  cout << "p" << p << endl;
   cout << one->value << " " << one->next << " "<<  two << endl;
+  
   // print the linked list value
   head = one;
   while (head != NULL) {
@@ -48,4 +55,41 @@ int main() {
     head = head->next;
   }
 }
+
+
+///////////////////////////////////////////////
+
+/*
+class Node{
+    public:
+    int data;
+    Node *nxt;
+};
+
+int main(){
+
+    //node define
+    Node *head;
+    Node * node1 = new Node;
+    Node * node2 = new Node;
+    Node * node3 = new Node;
+
+    //value assign 
+    node1->data = 4;
+    node2->data = 5;
+    node3->data = 6;
+
+    //connecting the nodes
+    node1->nxt = node2;
+    node2->nxt = node3;
+    node3->nxt = NULL;
+
+    cout << "print the node" << endl;
+    head = node1;
+    while(head != NULL){
+        cout << head->data << " ";
+        head = head->nxt;
+    }
+    
+}*/
 
---
+++ dsa14_linkedList/Single_Linkedlist.cpp
@@ -0,0 +1,101 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+
+    Node(int d){
+        this->data =d;
+        this->next = NULL;
+    }
+};
+
+void insetAtHead(Node* &head , int d){
+    //create new node
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d){
+
+    Node * temp = new Node(d);
+    tail->next = temp;
+    tail  = temp;
+
+}
+
+void print(Node* &head){
+
+    Node* temp = head; //temp is created because after the loop ,
+                        //its value become NULL
+    while(temp !=NULL){
+        cout << temp->data << " " ;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insetAtPost(Node * &head, int p , int d ){
+
+    //move till that position
+    Node * temp = head;
+    int cnt =1;
+    while(cnt <p -1){
+        temp = temp->next;
+        cnt++;
+    }
+    cout << "tempfinal " << temp->next << " " << temp->data << endl;
+
+    //attachment
+    Node * nodeMid = new Node(d);
+    nodeMid->next = temp->next;
+    temp->next = nodeMid;
+}
+
+void deleteNode(Node* &head , int p){
+    //take temp to that position
+    Node* temp = head;
+    int cnt =1;
+    while(cnt < p -1){
+        temp = temp->next;
+        cnt++;
+    }
+
+    // deleting
+    //attaching to nxt node
+    Node* curr = temp->next;
+    temp->next = curr->next;
+
+    //memory free
+    
+}
+
+int main(){
+    Node * one = new Node(10);  
+    Node * two = new Node(20);
+
+    //connect it
+    one->next = two;
+    two->next = NULL;
+    Node * head = one;
+    
+    print(head);
+    insetAtHead(head , 30);
+
+    Node* tail = two;
+    insertAtTail(tail , 40);
+
+    print(head);
+    insetAtPost(head , 4 , 100);
+    print(head);
+
+    cout << "head " << head->data << endl;
+    cout << "tail " << tail->data << endl;
+
+    deleteNode(head , 4);
+    print(head);
+
+}
