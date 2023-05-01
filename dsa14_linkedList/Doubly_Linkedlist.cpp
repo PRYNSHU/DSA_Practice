144882718fff2284d9cb4bc3c5229b9bf46a08a9
PRIYANSHU <4prianshu2051@gmail.com>
2023-05-01 22:56:44 +0530

    Lines

--- dsa14_linkedList/Doubly_Linkedlist.cpp
+++ dsa14_linkedList/Doubly_Linkedlist.cpp
@@ -7,11 +7,31 @@ class Node{
         Node* prev;
         Node* nxt;
     
-    
+    //cons
+    Node(int d){
+        this->data = d;
+        this->nxt = NULL;
+        this->prev = NULL;
+    }
 };
+
+void print(Node* head){
+    Node* temp = head;
+
+    while(temp!= NULL){
+        cout << temp->data << " ";
+        temp = temp->nxt;
+    }
+    cout << endl;
+}
 
 
 int main(){
 
-
+    Node* one = new Node(10);
+    Node* two = new Node(20);
+    one->nxt = two;
+    
+    Node* head = one;
+    print(head);
 }
---
+++ dsa14_linkedList/Questionset.md
@@ -0,0 +1,6 @@
+hi hello
+
+
+slkf 'a 
+sfsdfe
+<h3>Platforms in am using </h3>
