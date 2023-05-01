4d0889f22718d95ec81dd8829fe862cdd5f67121
PRIYANSHU <4prianshu2051@gmail.com>
2023-05-01 22:15:40 +0530

    anfads;la

--- dsa14_linkedList/Doubly_Linkedlist.cpp
+++ dsa14_linkedList/Doubly_Linkedlist.cpp
@@ -1,47 +1,17 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node{
     public:
         int data;
-        Node * pre;
-        Node * nxt;
-
-    //constructor call
-    Node(int d){
-        this->data = d;
-        this->pre = NULL;
-        this->nxt = NULL;
-    }
+        Node* prev;
+        Node* nxt;
+    
+    
 };
-
-void print(Node *head){
-
-    Node * temp = head;
-    int len = 0;
-    while(temp != NULL){
-        cout << temp->data << " ";
-        temp = temp->nxt;
-        len++;
-    }
-    cout << "length is " << len << endl;
-}
-
-void insertAtHead(Node* &head , int d){
-
-}
 
 
 int main(){
-    Node * one = new Node(10);
-    Node * two = new Node(20);
 
-    //connecting
-    one->nxt = two;
-    two->nxt = NULL;
-
-    Node * head = one;
-    print(head);
 
 }
-
