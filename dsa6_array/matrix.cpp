42ec427c467570ddff487898b7839d9b25208db6
PRIYANSHU <4prianshu2051@gmail.com>
2023-06-09 22:13:09 +0530

    array

---
+++ dsa6_array/matrix.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+
+	int arr[3][4] = { { 1, 2, 3, 4 },
+					{ 5, 6, 7, 8 },
+					{ 9, 10, 11, 12 }};
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
